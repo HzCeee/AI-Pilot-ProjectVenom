d4ccb9868a959db1bc75c854d81e671c284f2eeb
HzCee <huangzixihzx@gmail.com>
2018-03-20 14:56:29 -0400

    Config for exp4

--- DroneSimEnv_movingTarget.py
+++ DroneSimEnv_movingTarget.py
@@ -47,7 +47,7 @@ class DroneSimEnv(gym.Env):
         state related property
         '''
         # configuration of relative position in view
-        self.queue_length = 4
+        self.queue_length = 8
         self.coordinate_queue = None
         self.distance_queue = None
 
@@ -73,7 +73,7 @@ class DroneSimEnv(gym.Env):
                 self.min_roll, self.min_pitch, self.min_yaw, self.min_thrust,
                 self.min_relative_x, self.min_relative_y, self.min_relative_x, self.min_relative_y, self.min_relative_x, self.min_relative_y, self.min_relative_x, self.min_relative_y,
                 self.min_relative_x, self.min_relative_y, self.min_relative_x, self.min_relative_y, self.min_relative_x, self.min_relative_y, self.min_relative_x, self.min_relative_y,
-                self.min_distance, self.min_distance, self.min_distance, self.min_distance
+                self.min_distance, self.min_distance, self.min_distance, self.min_distance, self.min_distance, self.min_distance, self.min_distance, self.min_distance
             ]
         )
         self.high_state = np.array(
@@ -81,7 +81,7 @@ class DroneSimEnv(gym.Env):
                 self.max_roll, self.max_pitch, self.max_yaw, self.max_thrust,
                 self.max_relative_x, self.max_relative_y, self.max_relative_x, self.max_relative_y, self.max_relative_x, self.max_relative_y, self.max_relative_x, self.max_relative_y,
                 self.max_relative_x, self.max_relative_y, self.max_relative_x, self.max_relative_y, self.max_relative_x, self.max_relative_y, self.max_relative_x, self.max_relative_y,
-                self.max_distance, self.max_distance, self.max_distance, self.max_distance
+                self.max_distance, self.max_distance, self.max_distance, self.max_distance, self.max_distance, self.max_distance, self.max_distance, self.max_distance
             ]
         )
 
